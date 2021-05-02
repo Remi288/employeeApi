94a2cf46f46b38693f0138bfdd952dd5a9cb8226
Remi288 <iamopeoluwaadewale_28@yahoo.com>
2021-05-02 16:19:23 +0100

    endpoint-Test

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,76 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class BaseTest(TestCase):
+    def setUp(self):
+        self.register_url = reverse('register')
+        self.login_url = reverse('login')
+        self.user = {
+            'email': 'testemail@gmail.com',
+            'username': 'username',
+            'password': 'password',
+            'last_name': 'namee',
+            'first_name': 'name'
+        }
+        self.user_short_password = {
+            'email': 'testemail@gmail.com',
+            'username': 'username',
+            'password': 'pass',
+            'last_name': 'namee',
+            'first_name': 'name'
+        }
+
+        self.user_invalid_email = {
+
+            'email': 'testemail',
+            'username': 'username',
+            'password': 'password',
+            'last_name': 'namee',
+            'first_name': 'name'
+        }
+        return super().setUp()
+
+
+class RegisterTest(BaseTest):
+
+   def test_can_register_user(self):
+        response = self.client.post(
+            self.register_url, self.user)
+        self.assertEqual(response.status_code, 201)
+
+   def test_cant_register_user_withshortpassword(self):
+        response = self.client.post(
+            self.register_url, self.user_short_password)
+        self.assertEqual(response.status_code, 400)
+
+   def test_cant_register_user_with_invalid_email(self):
+        response = self.client.post(
+            self.register_url, self.user_invalid_email)
+        self.assertEqual(response.status_code, 400)
+
+   def test_cant_register_user_with_taken_email(self):
+        self.client.post(self.register_url, self.user)
+        response = self.client.post(
+            self.register_url, self.user)
+        self.assertEqual(response.status_code, 400)
+
+
+class LoginTest(BaseTest):
+
+    def test_login_success(self):
+        self.client.post(self.register_url, self.user)
+        response = self.client.post(
+            self.login_url, self.user)
+        self.assertEqual(response.status_code, 200)
+
+    def test_cantlogin_with_no_username(self):
+        response = self.client.post(
+            self.login_url, {'password': 'passwped', 'username': ''})
+        self.assertEqual(response.status_code, 401)
+
+    def test_cantlogin_with_no_password(self):
+        response = self.client.post(
+            self.login_url, {'username': 'passwped', 'password': ''})
+        self.assertEqual(response.status_code, 401)
--- employee/urls.py
+++ employee/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import EmployeeView, EmployeeDetailsView
 
 urlpatterns = [
-    path('employee/', EmployeeView.as_view()),
+    path('employee/', EmployeeView.as_view(), name="employee"),
     path('employee/<slug:employee_id>', EmployeeDetailsView.as_view()),
 
 ]
